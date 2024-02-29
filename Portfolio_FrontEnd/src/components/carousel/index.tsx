9452b25e3b03d71d04d84588bdeecfdf29aa5fcd
Eduardo Pereira <119636074+EduardoPereira7@users.noreply.github.com>
2024-02-29 00:10:06 +0000

    Add image loading functionality to Carousel component

--- Portfolio_FrontEnd/src/components/carousel/index.tsx
+++ Portfolio_FrontEnd/src/components/carousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ArrowLeft from "../../assets/svg/arrow-left-solid.svg";
 import ArrowRight from "../../assets/svg/arrow-right-solid.svg";
 import { ProjectImages } from "../../types/api/ProjectImages";
@@ -10,6 +10,45 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ projectImages }) => {
   const [current, setCurrent] = useState(0);
+  const [images, setImages] = useState(
+    new Array(projectImages.length).fill(null)
+  );
+  const [loading, setLoading] = useState(true);
+
+  const MAX_NUM_DISPLAY = 5;
+
+  const getDisplayNumbers = () => {
+    const totalImages = projectImages.length;
+    const numbers = [];
+
+    let start = Math.max(current - Math.floor(MAX_NUM_DISPLAY / 2), 0);
+    let end = Math.min(start + MAX_NUM_DISPLAY, totalImages);
+
+    // Adjust the start/end values if the last number is selected
+    if (end === totalImages && start > 0) {
+      start = Math.max(end - MAX_NUM_DISPLAY, 0);
+    }
+
+    for (let i = start; i < end; i++) {
+      numbers.push(i + 1);
+    }
+
+    return numbers;
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const img = new Image();
+    img.src = projectImages[current].link;
+    img.onload = () => {
+      setImages((images) => {
+        const newImages = [...images];
+        newImages[current] = img.src;
+        return newImages;
+      });
+      setLoading(false);
+    };
+  }, [current, projectImages]);
 
   const nextSlide = () => {
     setCurrent(current === projectImages.length - 1 ? 0 : current + 1);
@@ -26,38 +65,31 @@ const Carousel: React.FC<CarouselProps> = ({ projectImages }) => {
   return (
     <>
       <div className="carousel">
-        {projectImages.map((image, index) => (
-          <div
-            className={index === current ? "slide active" : "slide"}
-            key={index}
-          >
-            {index === current && (
-              <img
-                src={image.link}
-                alt="Imagem do Carousel"
-                className="image"
-              />
-            )}
-          </div>
-        ))}
+        <div className="slide active">
+          {loading ? (
+            <div className="loading_img">Loading...</div>
+          ) : (
+            <img src={images[current]} alt="Carousel Image" className="image" />
+          )}
+        </div>
       </div>
       <div className="arrows">
         <img
           src={ArrowLeft}
           width={40}
           height={40}
-          alt="Anterior"
+          alt="Previous"
           className="arrow"
           onClick={prevSlide}
         />
         <div className="image-counter">
-          {projectImages.map((_, index) => (
+          {getDisplayNumbers().map((index) => (
             <span
               key={index}
-              className={index === current ? "dot active-dot" : "dot"}
-              onClick={() => goToSlide(index)}
+              className={`dot ${index === current + 1 ? "active-dot" : ""}`}
+              onClick={() => goToSlide(index - 1)}
             >
-              {index + 1}
+              {index}
             </span>
           ))}
         </div>
@@ -65,7 +97,7 @@ const Carousel: React.FC<CarouselProps> = ({ projectImages }) => {
           src={ArrowRight}
           width={40}
           height={40}
-          alt="Próximo"
+          alt="Next"
           className="arrow"
           onClick={nextSlide}
         />
