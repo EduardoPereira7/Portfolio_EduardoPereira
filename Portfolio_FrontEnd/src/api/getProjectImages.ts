5c8e457318afe82d8105b26d268f52ae88f9fa13
Eduardo Pereira <119636074+EduardoPereira7@users.noreply.github.com>
2024-02-29 02:28:10 +0000

    Update API base URL

--- Portfolio_FrontEnd/src/api/getProjectImages.ts
+++ Portfolio_FrontEnd/src/api/getProjectImages.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/api";
+const baseURL = "http://13.39.162.91/api";
 
 export const getProjectImages = async (projectId: number) => {
   try {
